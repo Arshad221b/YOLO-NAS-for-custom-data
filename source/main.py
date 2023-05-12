10eab623f41cf420ff58de7ff06f18c6850d2daa
Arshad Kazi <kaziarshad97@gmail.com>
2023-05-12 20:09:38 +0530

    initial model training

---
+++ source/main.py
@@ -0,0 +1,17 @@
+import torch 
+import torch.nn as nn 
+from super_gradients.training import models
+
+model_path = '/Users/arshad_221b/Downloads/Projects/YOLO/traffic/models/ckpt_best.pth'
+
+model = models.get('yolo_nas_l',
+                        num_classes=4,
+                        checkpoint_path=model_path)
+
+input_path = '/Users/arshad_221b/Downloads/Projects/YOLO/traffic/inputs/test5.mp4'
+output_path = '/Users/arshad_221b/Downloads/Projects/YOLO/traffic/outputs/test5_output.mp4'
+
+device = 0 if torch.cuda.is_available() else "cpu"
+
+
+model.to(device).predict(input_path).save(output_path)
